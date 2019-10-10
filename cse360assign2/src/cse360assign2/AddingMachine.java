3357244f9b004c51e66d731b8fca4a837d398e49
Dana Lee <Dana Lee@DESKTOP-CGJ6OQF>
2019-10-09 18:33:57 -0700

    Inital assignment swtup

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,55 @@
+package cse360assign2;
+
+/**
+ * 
+ * @author Dana Lee
+ *
+ */
+public class AddingMachine {
+	private int total;
+	
+	/**
+	 * total is 0
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * @returns total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param value is added to total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value is subtracted to total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * returns an empty string
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/**
+	 * clears transaction history
+	 */
+	public void clear() {
+		
+	}
+	
+}
